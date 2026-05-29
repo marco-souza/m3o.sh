084a348ddd4b1270fec00cf99d9ef8b371240b73
Marco Souza <marco.souza@mongodb.com>
2026-05-29 10:37:55 -0300

    enh: upgrade depedencies

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: cloudflare(),
 
   fonts: [
