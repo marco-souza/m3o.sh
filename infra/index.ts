c0b778ecd98886ef83c5b05e029ac83bc8b0baab
Marco Antônio <ma.souza.junior@gmail.com>
2026-07-29 19:15:28 -0300

    fix: disable cross version cache

--- infra/index.ts
+++ infra/index.ts
@@ -66,7 +66,7 @@ const workerVersion = new cloudflare.WorkerVersion(
 
     cacheOptions: {
       enabled: true,
-      crossVersionCache: true,
+      crossVersionCache: false,
     },
 
     assets: {
