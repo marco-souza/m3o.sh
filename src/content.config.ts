dab69969e10839e677d5ba3d106e571d33122de3
Marco Souza <marco.souza@mongodb.com>
2026-09-23 11:12:39 -0300

    fea: create blog and authors collections

---
+++ src/content.config.ts
@@ -0,0 +1,31 @@
+import { defineCollection, reference } from "astro:content";
+
+import { file, glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    author: reference("authors"),
+    publishedAt: z.coerce.date(),
+    updatedAt: z.coerce.date().optional(),
+  }),
+});
+
+const authors = defineCollection({
+  loader: file("./src/data/authors.json"),
+  schema: z.object({
+    id: z.string(),
+    name: z.string(),
+    headline: z.string(),
+    email: z.email(),
+    avatar: z.url(),
+  }),
+});
+
+export const collections = {
+  blog,
+  authors,
+};
---
+++ src/content/blog/old-programmer-yelling-at-claude.md
@@ -0,0 +1,13 @@
+---
+title: Old programmer yelling at Claude
+description: this is a rant about AI culture
+author: marco.souza
+
+publishedAt: 2026-09-23
+updatedAt: 2026-09-23
+---
+
+## Old programmer yelling at Claude
+
+This post is another AI rant, from a personal standpoint of a person who lived
+to code until the AI culture took the joy away.
